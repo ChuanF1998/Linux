0798217dfdc46b9ab891e4f2e67b1e8bd17b19d6
ChuanF1998 <895952166@qq.com>
2020-02-20 17:55:48 +0800

    线程池代码

--- threadpool/threadpool.cpp
+++ threadpool/threadpool.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
@@ -47,22 +48,91 @@ public:
     _is_quit = false;
 
     //创建线程
-    Threadcreate();
+    bool is_create = Threadcreate();
+    if (!is_create) {
+      std::cout << "Thread creare failed !" << std::endl;
+      exit(1);
+    }
   }
 
+  //放数据
+  bool push(ThreadTask* t)
+  {
+    pthread_mutex_lock(&_mutex);
+    if (_is_quit) {
+      pthread_mutex_unlock(&_mutex);
+      return false;
+    }
+    _que.push(t);
+    pthread_mutex_unlock(&_mutex);
+    //放完数据的时候需要通知线程池当中的线程
+    pthread_cond_signal(&_cond);
+    return true;
+  }
+
+  //取数据
+  bool pop(ThreadTask** t)
+  {
+    *t = _que.front();
+    _que.pop();
+    return true;
+  }
+
+  bool PoolClrear()
+  {
+    pthread_mutex_lock(&_mutex);
+    _is_quit = true;
+    pthread_mutex_unlock(&_mutex);
+    //当线程当中的队列没有数据时，线程都是阻塞在锁中
+    if (_thread_curnum > 0) {
+      pthread_cond_broadcast(&_cond);
+    }
+    return true;
+  }
+
+  //线程入口函数
+private:
+  static void* ThreadStart(void* arg)
+  {
+    Threadpool* t = (Threadpool*)arg;
+    while (1) {
+      pthread_mutex_lock(&t->_mutex);
+      while (t->_que.empty()) {
+        //如果不在此位置，则会一直阻塞
+        if (t->_is_quit) {
+          t->ThreadQuit();
+        }
+        pthread_cond_wait(&t->_cond, &t->_mutex);
+      }
+      ThreadTask* v;
+      t->pop(&v);
+      pthread_mutex_unlock(&t->_mutex);
+      //由于已经拿到数据了，所以可以先行把锁释放
+      v->run();
+    }
+    return NULL;
+  }
+
+  void ThreadQuit()
+  {
+    //临界
+    _thread_curnum--;
+    pthread_mutex_unlock(&_mutex);
+    pthread_exit(NULL);
+  }
 private:
   bool Threadcreate()
   {
     int ret = -1;
     pthread_t tid;
     for (int i = 0; i < THREADCOUNT; ++i) {
-      ret = pthread_create(&tid, NULL, ThreadStart, NULL);
+      ret = pthread_create(&tid, NULL, ThreadStart, (void*)this);
       if (ret != 0) {
         std::cout << "create thread failed !" << std::endl;
         return false;
       }
     }
-    return false;
+    return true;
   }
 
 private:
@@ -82,7 +152,20 @@ private:
 
 std::queue<ThreadTask*> que;
 
+void print(int Data)
+{
+  std::cout << "I am a Handler, ThreadTask " << Data << std::endl;
+}
+
 int main()
 {
+  Threadpool* t = new Threadpool();
+  for (int i = 0; i < 10; ++i) {
+    ThreadTask* v = new ThreadTask(i, print);
+    t->push(v);
+  }
+  sleep(5);
+  t->PoolClrear();
+  delete t;
   return 0;
 }
