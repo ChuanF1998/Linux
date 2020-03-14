c830130013a59d9a51d94c89c61342d7bbb465bf
ChuanF1998 <895952166@qq.com>
2020-03-15 01:00:30 +0800

    提交

--- pcb/getenv.c
+++ pcb/getenv.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main()
+int main(int argc, char *argv[], char *env[])
 {
-  char * env = getenv("MYENV");
-  if(env){
-    printf("%s\n", env);
+  int i = 0;
+  for(; env[i]; i++){
+    printf("%s\n", env[i]);
 
   }
   return 0;
